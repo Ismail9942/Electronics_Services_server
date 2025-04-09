5d371a52a735dc025485261f2b5bf5cab453407e
Ismail9942 <ismailhossainreza@gmail.com>
2025-04-09 22:36:01 +0600

    single id get

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.llrud.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,12 +28,22 @@ async function run() {
     app.post("/service", async (req, res) => {
       const addService = req.body;
       const result = await servicesCollection.insertOne(addService);
-      console.log(result);
       res.send(result);
     });
     // get all services from db
     app.get("/services", async (req, res) => {
+      const result = await servicesCollection.find().limit(6).toArray();
+      res.send(result);
+    });
+    app.get("/allServices", async (req, res) => {
       const result = await servicesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
       res.send(result);
     });
 
